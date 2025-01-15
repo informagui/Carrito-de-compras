841133ad0687c4578c251b3810d62d83af747cc6
Magali <magali.krueel@gmail.com>
2025-01-14 21:29:37 -0300

    agregando estilos

--- product-list-with-cart-main/tailwind.config.js
+++ product-list-with-cart-main/tailwind.config.js
@@ -9,7 +9,9 @@ module.exports = {
 
       colors: {
         realRed: 'var(--real-red)',
-        realGreen: 'var(--real-green)'
+        realGreen: 'var(--real-green)',
+        rose50: 'var(--Rose-50)',
+        rose100: 'var(--Rose-100)'
       }
     },
   },
