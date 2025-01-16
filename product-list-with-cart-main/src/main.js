ff849b8f5d3e5955b2c9857b31151708cfbc4ba1
Magali <magali.krueel@gmail.com>
2025-01-15 21:26:30 -0300

    un poquito de funcion a los botones js

--- product-list-with-cart-main/src/main.js
+++ product-list-with-cart-main/src/main.js
@@ -8,11 +8,46 @@ function addProduct(id){
     cantProductosEl.innerText = cantProductos;
     const producto = document.getElementById(id);
     const button =producto.querySelector('button.add-cart');
+    button.removeAttribute('onclick');
     button.classList.add('bg-realRed');
     button.innerHTML = `
         <button class="decrement border border-white text-white rounded-full w-6 h-6 font-bold" onclick="decrement(${id})">-</button>
         <span class="quantity text-white font-bold mx-6">1</span>
         <button class="increment border border-white text-white rounded-full w-6 h-6 font-bold" onclick="increment(${id})">+</button>
     `;
-      
 }
+
+function decrement(id){
+    const producto = document.getElementById(id);
+    const cantidadEl = producto.querySelector('.quantity');
+    const cantidad = cantidadEl.innerText 
+    if (cantidad == 1){
+        resetButton(id)
+    } else {
+        cantidadEl.innerText = cantidad - 1;
+    }
+
+    cantProductos --;
+    cantProductosEl.innerText = cantProductos;
+}
+
+function increment(id){
+    const producto = document.getElementById(id);
+    const cantidadEl = producto.querySelector('.quantity');
+    let cantidad = cantidadEl.innerText;
+    cantidad ++;
+    cantidadEl.innerText = cantidad;
+    cantProductos ++;
+    cantProductosEl.innerText = cantProductos;
+}
+
+function resetButton(productId) {
+const container = document.getElementById(productId);
+const button = container.querySelector('button');
+button.className = 'add-cart'; // Restaurar las clases originales
+button.innerHTML = `
+    <img src="./assets/images/icon-add-to-cart.svg" alt="">
+    Add to cart
+`;
+}
+  
