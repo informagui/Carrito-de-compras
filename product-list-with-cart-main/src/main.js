4faafe155e4da201b9b16b74bbc1025b786dbe03
Magali <magali.krueel@gmail.com>
2025-01-16 18:02:55 -0300

    se agregan los productos al carrito js (basic)

--- product-list-with-cart-main/src/main.js
+++ product-list-with-cart-main/src/main.js
@@ -1,13 +1,29 @@
+let isCartEmpty = true;
 const cart = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+let productos = [];
 let cantProductos = 0;
 let cantProductosEl= document.getElementById("cant-productos");
 
+fetch('../data.json')
+  .then(response => response.json())
+  .then(data => {
+    productos = data;
+  })
+  .catch(error => console.error('Error al cargar los datos:', error));
+
+
+
 function addProduct(id){
+    //Cambios generales
     cart[id-1] ++;
     cantProductos ++;
+
+    //Obteniendo html
     cantProductosEl.innerText = cantProductos;
     const producto = document.getElementById(id);
     const button =producto.querySelector('button.add-cart');
+
+    //Cambiando el boton de add to cart
     button.removeAttribute('onclick');
     button.classList.add('bg-realRed');
     button.innerHTML = `
@@ -15,6 +31,39 @@ function addProduct(id){
         <span class="quantity text-white font-bold mx-6">1</span>
         <button class="increment border border-white text-white rounded-full w-6 h-6 font-bold" onclick="increment(${id})">+</button>
     `;
+
+    //Cambiando el carrito
+    addToCart(id);
+}
+
+function addToCart(id){
+    const postre = productos[(id -1)];
+    const cartContainer = document.querySelector('#cart');
+
+    if (isCartEmpty === true){
+        isCartEmpty = false;
+        let emptyCartEl = document.getElementById("empty-cart");
+        cartContainer.removeChild(emptyCartEl);
+    }
+
+    const productDiv = document.createElement('div');
+    productDiv.className = 'product-in-cart';
+    productDiv.setAttribute('data-id', id);
+    
+    // Generar el contenido del producto
+    productDiv.innerHTML = `
+        <div>
+        <p class="name">${postre.name}</p>
+        <p class="dessert">
+            <span class="price">x1</span> @$${postre.price.toFixed(2)} 
+            <span class="font-semibold"> $${(postre.price * 1).toFixed(2)}</span>
+        </p>
+        </div>
+        <span class="borrar-prod" onclick="removeFromCart(${id})">x</span>
+    `;
+    
+    // Agregar el producto al carrito
+    cartContainer.appendChild(productDiv);
 }
 
 function decrement(id){
@@ -29,6 +78,10 @@ function decrement(id){
 
     cantProductos --;
     cantProductosEl.innerText = cantProductos;
+
+    if (cantProductos === 0){
+        carritoVacio();
+    }
 }
 
 function increment(id){
@@ -39,15 +92,31 @@ function increment(id){
     cantidadEl.innerText = cantidad;
     cantProductos ++;
     cantProductosEl.innerText = cantProductos;
+    addToCart(id);
 }
 
 function resetButton(productId) {
-const container = document.getElementById(productId);
-const button = container.querySelector('button');
-button.className = 'add-cart'; // Restaurar las clases originales
-button.innerHTML = `
-    <img src="./assets/images/icon-add-to-cart.svg" alt="">
-    Add to cart
-`;
+    const producto = document.getElementById(productId);
+    const button = producto.querySelector('button');
+    button.className = 'add-cart';
+    button.innerHTML = `
+        <img src="./assets/images/icon-add-to-cart.svg" alt="">
+        Add to cart
+    `;
 }
-  
+ 
+function carritoVacio(){
+    isCartEmpty = true;
+    const cartContainer = document.querySelector('#cart');
+    cartContainer.innerHTML = '';
+    
+    // Agrega el mensaje de carrito vacío
+    cartContainer.innerHTML = `
+        <h3 class="text-realRed font-bold text-lg lg:text-2xl mb-3"> Your Cart ( <span id="cant-productos">0</span> )</h3>
+        <div id="empty-cart">
+        <img src="./assets/images/illustration-empty-cart.svg" alt="" class="mx-auto">
+        <p class="text-sm text-center lg:mt-2 lg:text-base">Your added items will appear here</p>
+        </div>
+    `;      
+    cantProductosEl = document.getElementById("cant-productos");
+}
