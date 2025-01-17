3f891cc023515e7e1b20a6de67dc9aa4adc0e477
Magali <magali.krueel@gmail.com>
2025-01-17 15:34:14 -0300

    terminando c/producto del cart en js

--- product-list-with-cart-main/src/main.js
+++ product-list-with-cart-main/src/main.js
@@ -37,41 +37,44 @@ function addProduct(id){
 }
 
 function addToCart(id){
+    //Getting the needed html
     const postre = productos[(id -1)];
     const cartContainer = document.querySelector('#cart');
 
+    //Removing empty cart style
     if (isCartEmpty === true){
         isCartEmpty = false;
         let emptyCartEl = document.getElementById("empty-cart");
         cartContainer.removeChild(emptyCartEl);
     }
 
+    //Creating the product for the cart
     const productDiv = document.createElement('div');
     productDiv.className = 'product-in-cart';
     productDiv.setAttribute('data-id', id);
     
-    // Generar el contenido del producto
     productDiv.innerHTML = `
         <div>
         <p class="name">${postre.name}</p>
         <p class="dessert">
-            <span class="price">x1</span> @$${postre.price.toFixed(2)} 
-            <span class="font-semibold"> $${(postre.price * 1).toFixed(2)}</span>
+            <span class="price quantity-of-${id}">x1</span> @$${postre.price.toFixed(2)} 
+            <span class="font-semibold ml-2 price-of-${id}"> $${postre.price.toFixed(2)}</span>
         </p>
         </div>
         <span class="borrar-prod" onclick="removeFromCart(${id})">x</span>
     `;
     
-    // Agregar el producto al carrito
+    // Adding the product
     cartContainer.appendChild(productDiv);
 }
 
 function decrement(id){
+    cart[id -1] --;
     const producto = document.getElementById(id);
     const cantidadEl = producto.querySelector('.quantity');
     const cantidad = cantidadEl.innerText 
     if (cantidad == 1){
-        resetButton(id)
+        removeFromCart(id)
     } else {
         cantidadEl.innerText = cantidad - 1;
     }
@@ -82,17 +85,48 @@ function decrement(id){
     if (cantProductos === 0){
         carritoVacio();
     }
+
+    //Decrementing Cart
+    const cartContainer = document.querySelector('#cart');
+    const cantidadEnCarritoEl = cartContainer.querySelector('.quantity-of-' + id);
+    cantidadEnCarritoEl.innerText = "x" + cart[id-1];
+    const precioSubtotalEl = cartContainer.querySelector('.price-of-' + id);
+    precioSubtotalEl.innerText = "$" + (productos[(id - 1)].price * cart[id-1]).toFixed(2);
 }
 
 function increment(id){
+    cart[id-1] ++;
+    cantProductos ++;
+
+    //Changing html
     const producto = document.getElementById(id);
     const cantidadEl = producto.querySelector('.quantity');
     let cantidad = cantidadEl.innerText;
     cantidad ++;
     cantidadEl.innerText = cantidad;
-    cantProductos ++;
     cantProductosEl.innerText = cantProductos;
-    addToCart(id);
+
+    //Incrementing Cart
+    const cartContainer = document.querySelector('#cart');
+    const cantidadEnCarritoEl = cartContainer.querySelector('.quantity-of-' + id);
+    cantidadEnCarritoEl.innerText = "x" + cart[id-1];
+    const precioSubtotalEl = cartContainer.querySelector('.price-of-' + id);
+    precioSubtotalEl.innerText = "$" + (productos[(id - 1)].price  * cart[id-1]).toFixed(2);
+}
+
+function removeFromCart(id){
+    cantProductos -= cart[id-1];
+    cantProductosEl.innerText = cantProductos;
+    cart[id-1] = 0;
+    if (cantProductos === 0){
+        carritoVacio();
+    }
+    resetButton(id);
+    
+    //Removing from cart html
+    const cartContainer = document.querySelector('#cart');
+    const productToRemove= cartContainer.querySelector(`.product-in-cart[data-id="${id}"]`);
+    cartContainer.removeChild(productToRemove);
 }
 
 function resetButton(productId) {
